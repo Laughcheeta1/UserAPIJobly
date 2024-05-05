9da88c0ec9e1e0746c9cb334d5108aa7ae30db21
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2024-05-04 19:28:40 -0500

    Made the UserRepositoryFile

--- DatabaseLayer/UserRepository.js
+++ DatabaseLayer/UserRepository.js
@@ -1,10 +1,20 @@
 const connectDb = require('../db.js');
 
+const basicInfoMethods = require('./BasicInfoMethods.js');
+const extraInfoMethods = require('./ExtraInfoMethods.js');
+const certificationsMethods = require('./CertificationsMethods.js');
+const reviewsMethods = require('./ReviewsMethods.js');
+const servicesMethods = require('./ServicesMethods.js');
+
 const initialize = async () => {
     db = await connectDb();
 
     return {
-
+        ...basicInfoMethods(db),
+        ...extraInfoMethods(db),
+        ...certificationsMethods(db),
+        ...reviewsMethods(db),
+        ...servicesMethods(db)
     };
 };
 
